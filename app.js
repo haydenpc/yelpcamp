11b184771333cf3fa34c032b09d17bb5b973e7fb
Hayden <haydenpc369@gmail.com>
2024-08-20 11:36:11 -0500

    use enviroment var for db

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")
 
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
 // 'mongodb://127.0.0.1:27017/yelp-camp'
 
 mongoose.connect(dbUrl, {
